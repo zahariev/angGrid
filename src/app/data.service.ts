fba9c5518dac945c4c518f2de763bfdb4d1a8890
boyan zahariev <zaharievboyan@gmail.com>
2020-01-24 00:17:26 +0200

    add: observable object rowFilter

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private rowFilter = new BehaviorSubject(new Array());
+  filter = this.rowFilter.asObservable();
 
-  constructor() { }
+  rows: any [] = [];
+  selected = [];
+
+  constructor() {
+
+  }
+
+  fetch(cb) {
+    const req = new XMLHttpRequest();
+    req.open('GET', `assets/100k.json`);
+
+    req.onload = () => {
+      cb(JSON.parse(req.response));
+    };
+
+    req.send();
+  }
+
+  setFilter(row) {
+    this.rowFilter.next(row);
+  }
+
 }
