eb66ab64dac8fc07fd82d490396ad4c40b5e6576
boyan zahariev <zaharievboyan@gmail.com>
2020-01-24 08:02:39 +0200

    add: weekly sales data api

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -15,12 +15,24 @@ export class DataService {
 
   }
 
-  fetch(cb) {
+  fetchDetails(cb) {
     const req = new XMLHttpRequest();
-    req.open('GET', `assets/100k.json`);
+    req.open('GET', `https://api.bilkata.bg/info/dashboard/SalesDetails/2020-01-23?ApiKey=0dd46cac448dce1591f7993b79bc8785`);
 
     req.onload = () => {
-      cb(JSON.parse(req.response));
+      cb(JSON.parse(req.response).week);
+    };
+
+    req.send();
+  }
+
+  fetchMain(cb) {
+    const req = new XMLHttpRequest();
+
+    req.open('GET', `https://api.bilkata.bg/info/dashboard/SalesByCategory/2020-01-23?ApiKey=0dd46cac448dce1591f7993b79bc8785`);
+    // https://restcountries.eu/rest/v2/all
+    req.onload = () => {
+      cb(JSON.parse(req.response).today);
     };
 
     req.send();
