6f96461c1e164e5ff30da103415f8fbb72c90a9f
boyan zahariev <zaharievboyan@gmail.com>
2020-01-26 00:06:35 +0200

    fix: css hide 0column on desktop mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { AppComponent } from './app.component';
-import { GridComponent } from './grid/grid.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DetailGridComponent } from './detail-grid/detail-grid.component';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {AppComponent} from './app.component';
+import {GridComponent} from './grid/grid.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DetailGridComponent} from './detail-grid/detail-grid.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,11 @@ import { DetailGridComponent } from './detail-grid/detail-grid.component';
   ],
   imports: [NgxDatatableModule,
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
