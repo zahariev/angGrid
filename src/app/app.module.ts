8959fdbcbb2b8371423b515c71de7bd6979e06c1
boyan zahariev <zaharievboyan@gmail.com>
2020-01-26 17:54:56 +0200

    details grid initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,20 @@ import {GridComponent} from './grid/grid.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {DetailGridComponent} from './detail-grid/detail-grid.component';
 import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule, MatIconModule} from '@angular/material';
+import {DetailsFormComponent} from './details-form/details-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
-    DetailGridComponent
+    DetailGridComponent,
+    DetailsFormComponent
   ],
   imports: [NgxDatatableModule,
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, MatIconModule, MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
